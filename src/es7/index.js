d7098b221203816b0db291becf0a2fbb6c9a9cdb
Ernesto Martinez <ernestomm99@hotmail.com>
2021-03-09 12:12:00 -0700

    es7

---
+++ src/es7/index.js
@@ -0,0 +1,13 @@
+let numbers = [1,2,3,7,8];
+
+if(numbers.includes(9)) {
+    console.log('Existe un 7 OMFG');
+} else {
+    console.log('Like god, it doesnt exist');
+}
+
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+console.log(result);
